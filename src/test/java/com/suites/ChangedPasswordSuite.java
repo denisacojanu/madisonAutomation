dea04dd0f319be3c5be31a584e69c0b763f58300
denisacojanu <denisan.cojanu@gmail.com>
2022-08-18 21:20:03 +0300

    Add change password suite

---
+++ src/test/java/com/suites/ChangedPasswordSuite.java
@@ -0,0 +1,22 @@
+package com.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.tests.account.ChangePasswordTest;
+import com.tests.account.InvalidCurrentPasswordTest;
+import com.tests.search.SearchProductGuestTest;
+import com.tests.search.SearchProductLoggedUserTest;
+
+@SuiteClasses({
+	
+	ChangePasswordTest.class,
+	InvalidCurrentPasswordTest.class
+
+})
+
+@RunWith(Suite.class)
+public class ChangedPasswordSuite {
+
+}
--- src/test/java/com/suites/RegressionSuite.java
+++ src/test/java/com/suites/RegressionSuite.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 		RegisterSuite.class,
 		LoginSuite.class, 
-		SearchSuite.class
+		SearchSuite.class,
+		ChangedPasswordSuite.class
 
 })
 
