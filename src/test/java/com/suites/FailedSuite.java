50b59ef1f15cbbe5af5f9838f7083c72aa6d8f7c
denisacojanu <denisan.cojanu@gmail.com>
2022-08-19 00:15:24 +0300

    Add failed suite

---
+++ src/test/java/com/suites/FailedSuite.java
@@ -0,0 +1,21 @@
+package com.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.tests.login.ValidLoginTest;
+import com.tests.search.SearchProductGuestTest;
+import com.tests.search.SearchProductLoggedUserTest;
+
+@SuiteClasses({
+	
+	SearchProductGuestTest.class,
+	ValidLoginTest.class
+
+})
+
+@RunWith(Suite.class)
+public class FailedSuite {
+
+}
